0705bb6b78190a258241302f279dfe6631a8b9ad
izaelsilva1 <izael.silva@tempoassist.com.br>
2025-05-22 22:37:12 -0300

    implementar modelo de DOMINIO -ics

---
+++ src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
@@ -0,0 +1,80 @@
+package com.devsuperior.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+
+//comando para incapsular dois atributos numa unica classe.
+@Embeddable
+
+public class BelongingPK {
+	// classe relacional, contem relaciomannto entre as chaves primarias das tabelas Games e GameList;
+	
+	@ManyToOne
+	@JoinColumn(name = "game_id")
+	private Game game;
+	
+	@ManyToOne
+	@JoinColumn(name = "list_id")
+	private GameList list;
+	
+	
+	public BelongingPK() {
+		
+	}
+
+
+	public BelongingPK(Game game, GameList list) {
+		this.game = game;
+		this.list = list;
+	}
+
+
+	public Game getGame() {
+		return game;
+	}
+
+
+	public void setGame(Game game) {
+		this.game = game;
+	}
+
+
+	public GameList getList() {
+		return list;
+	}
+
+
+	public void setList(GameList list) {
+		this.list = list;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(game, list);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BelongingPK other = (BelongingPK) obj;
+		return Objects.equals(game, other.game) && Objects.equals(list, other.list);
+	}
+	
+	
+	
+	
+	
+	
+
+}
